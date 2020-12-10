e70f3392660aea98d44158b5ffbd0897c4246ed2
Deepak Gahlot <depak.gahlot@sap.com>
2020-12-10 21:12:10 +0530

    adding glide
    adding glide logic to display images

--- app/src/main/java/com/example/coroutinessample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinessample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.coroutinessample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
@@ -12,8 +13,8 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
-    private val DUMMY_DATA = "Result is being printed"
-    private val DUMMY_DATA2 = "Second Result is being printed"
+    private val DUMMY_DATA = "Setting image 1"
+    private val DUMMY_DATA2 = "Setting image 2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,10 +27,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private suspend fun setImageOnMainThread(imageUrl: String) {
+        withContext(Main) {
+            Glide.with(applicationContext).load(imageUrl).into(imageview)
+        }
+    }
+
     private suspend fun setTextOnMainThread(input: String) {
         withContext(Main) {
-            val newText = textview.text.toString() + "\n$input"
-            textview.text = newText
+            textview.text = input
         }
     }
 
@@ -37,10 +43,14 @@ class MainActivity : AppCompatActivity() {
         val result1 = getResultDummyCall()
         println("debug: $result1")
         setTextOnMainThread(result1)
+        delay(1000)
+        setImageOnMainThread("https://homepages.cae.wisc.edu/~ece533/images/mountain.png")
 
         val result2 = getResultDummyCall1()
         println("debug: $result2")
         setTextOnMainThread(result2)
+        delay(2000)
+        setImageOnMainThread("https://homepages.cae.wisc.edu/~ece533/images/airplane.png")
     }
 
     //delay will only delay this single coroutine not the entire thread
