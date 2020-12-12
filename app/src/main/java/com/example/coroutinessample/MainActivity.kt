3ff1a17a500ffcc011a5be03fabd0f6ace3fd973
Deepak Gahlot <depak.gahlot@sap.com>
2020-12-12 22:21:13 +0530

    adding time out function
    Adding function for the timeout button

--- app/src/main/java/com/example/coroutinessample/MainActivity.kt
+++ app/src/main/java/com/example/coroutinessample/MainActivity.kt
@@ -4,12 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +20,17 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             CoroutineScope(IO).launch {
                 fakeDummyCall()
+            }
+        }
+
+        button2.setOnClickListener {
+            CoroutineScope(IO).launch {
+                val job = withTimeoutOrNull(2000) {
+                    fakeDummyCall()
+                }
+                if (job == null) {
+                    setTextOnMainThread("Request taking longer than expected")
+                }
             }
         }
     }
